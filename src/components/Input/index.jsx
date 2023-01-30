9ee4e3ce0e3444997b3bd646595fb66a2070b5c8
AndyGuit <totalslash@msn.com>
2023-01-31 01:36:15 +0300

    feat: add input component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
+import styles from './Input.module.scss';
 
 const Input = () => {
   const [value, setValue] = useState('');
 
   return (
-    <div>
+    <div className={styles.inputBlock}>
       <input
         type="text"
+        placeholder="Search for any IP address or domain"
         value={value}
         onInput={e => setValue(e.target.value)}
       />
-      <button></button>
+      <button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+          <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+        </svg>
+      </button>
     </div>
   );
 };
