3cf72c0f94c018d6740dbcf91eb6feb2ef4d0cf3
AndyGuit <totalslash@msn.com>
2023-01-30 07:54:59 +0300

    feat: add basic styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import './scss/app.scss';
+
 function App() {
-  return <div className="App">IP address tracker</div>;
+  return (
+    <div className="App">
+      <h1>IP Address Tracker</h1>
+    </div>
+  );
 }
 
 export default App;
